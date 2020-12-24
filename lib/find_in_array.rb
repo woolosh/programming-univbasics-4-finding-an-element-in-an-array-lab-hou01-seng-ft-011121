b8a44abd97dd80c64488a00de283c661d7543391
Joshua Morales <joshuapmorales@gmail.com>
2020-12-24 04:36:53 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,7 @@
 def find_element_index(array, value_to_find)
  
- count = 0 
- 
- while count < array.length do
-   puts array[count] == value_to_find
-   count += 1 
+ if array.include?(value_to_find) do
+   return array[value_to_find]
  end
  
 end
